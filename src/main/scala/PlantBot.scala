7d5cad1e941a23e9c3833922ec688c7a52f72203
Mendor <mendor@yuuzukiyo.net>
2017-01-28 17:33:04 +0200

    Some code cleanup

--- src/main/scala/PlantBot.scala
+++ src/main/scala/PlantBot.scala
@@ -1,11 +1,9 @@
 package net.yuuzukiyo.plantbot
 
 import scala.io.Source
-import scala.util.Properties
 import java.io._
-import java.nio.file.{Files, Paths}
-import net.sourceforge.plantuml.SourceStringReader
 import info.mukel.telegrambot4s._, api._, methods._, models._, Implicits._
+import net.sourceforge.plantuml.SourceStringReader
 
 
 object PlantBot extends App
@@ -16,14 +14,17 @@ object PlantBot extends App
   lazy val token = Source.fromFile("bot.token").getLines().mkString
 
   override def onMessage(msg: Message): Unit = {
+    // read original message text
     val input = new StringBuilder
-    for (text <- msg.text) {
+    for (text <- msg.text)
       input ++= text
-    }
+
+    // generate PNG from the description
     val pngOutput = new ByteArrayOutputStream
     val reader = new SourceStringReader(input.toString)
     val desc = reader.generateImage(pngOutput)
 
+    // send PNG back
     val pngSend = InputFile("plant.png", pngOutput.toByteArray)
     request(SendPhoto(msg.sender, pngSend))
   }
